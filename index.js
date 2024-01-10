ccd17f58c7df654a3c105cdcce860dc42b637a5a
unknown <BT_5E7_03@BKTECH.LOCAL>
2024-01-10 15:08:12 -0500

    aaaa

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const buttonTemplate = "<button class=\"numberButton\" id=\"temp\">temp</button>";
 const grid = document.getElementById("grid");
 function initialize(){
     for(i = 1; i <= 15; i++) {
         grid.appendChild(generateButton(i));
     }
     grid.appendChild(generateButton("space"))
+    const buttons = Array.from(grid.getElementsByClassName("numberButton"))
+    buttons.forEach(button => {
+        button.addEventListener("click", (button) => onSelect(button.target));
+    })
+
+}
+function swapPlaces(buttonOne, buttonTwo) {
+    let tempArea = buttonOne.style.gridArea;
+    console.log(tempArea);
+}
+
+function onSelect(button) {
+   swapPlaces(button, document.getElementById("space"));
 }
 function generateButton(text){
     let newButton = document.createElement("button");
@@ -12,7 +24,15 @@ function generateButton(text){
     newButton.id = `${text}`;
     if(text != "space"){
         newButton.innerText = `${text}`;
+        newButton.style.gridRow = Math.trunc(Number.parseInt(text) / 4 + 1);
+        newButton.style.gridColumn;
+        console.log(Math.trunc(Number.parseInt(text) / 4));
+    } else {
+        newButton.style.gridColumn = 4
+        newButton.style.gridRow = 4
     }
+    
     return newButton;
 }
+
 initialize();
