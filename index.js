c79e23f8a73b5c66a7e73d436baaf82e763a29d1
unknown <BT_5E7_03@BKTECH.LOCAL>
2024-01-18 15:22:18 -0500

    dhewuhdiwe

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const grid = document.getElementById("grid");
+const resetButton = document.getElementById("reset");
+const startButton = document.getElementById("start");
+const scoreText = document.getElementById("scoreNum");
+const totalPieces = 16;
+let score = 0;
+let gameStarted = false;
 // util functions
 
-/*[
+/*
     Gets grid area formatted in an Number[]
     [row, column]
 */
@@ -12,7 +18,7 @@ function getGridArea(element) {
 // game functions
 
 function initialize(){
-    for(i = 1; i <= 15; i++) {
+    for(i = 1; i <= totalPieces - 1; i++) {
         grid.appendChild(generateButton(i));
     }
     grid.appendChild(generateButton("space"))
@@ -20,7 +26,49 @@ function initialize(){
     buttons.forEach(button => {
         button.addEventListener("click", (button) => onSelect(button.target));
     })
+    resetButton.addEventListener("click", reset);
+    startButton.addEventListener("click", toggleGameStarted);
 }
+
+function reset() {
+    Array.from(grid.getElementsByClassName("numberButton")).forEach(button => {
+        if(button.id != "space") {
+            let number = Number.parseInt(button.id);
+            button.style.gridRow = Math.trunc((number - 1) / 4 + 1);
+            button.style.gridColumn = number - ((Number.parseInt(button.style.gridRow) - 1) * 4);
+        } else {
+            button.style.gridArea = "4 / 4";
+        }
+    })
+}
+
+function update() {
+    scoreText.textContent = score;
+}
+
+function checkForCompletion(){
+    let gameWon = true;
+    Array.from(grid.getElementsByClassName("numberButton")).forEach(button => {
+        if(button.id != "space") {
+            let number = Number.parseInt(button.id);
+            let gridArea = getGridArea(button);
+            if(gridArea[0] != Math.trunc((number - 1) / 4 + 1) 
+            || gridArea[1] != number - ((Number.parseInt(button.style.gridRow) - 1) * 4)){
+                gameWon = false;
+            }
+        } else {
+            if(document.getElementById("space").style.gridArea != "4 / 4"){
+                gameWon = false;
+            }
+        }
+    })
+    return gameWon;
+}
+
+function onGameWin(){
+    console.log("You won!");
+}
+
 function swapPlaces(buttonOne, buttonTwo) {
     let tempArea = buttonOne.style.gridArea;
     buttonOne.style.gridArea = buttonTwo.style.gridArea;
@@ -34,7 +82,15 @@ function onSelect(button) {
     || (buttonArea[1] == spaceArea[1] && (buttonArea[0] - 1 == spaceArea[0] || buttonArea[0] + 1 == spaceArea[0]))) {
             swapPlaces(button, document.getElementById("space"));
         }
+    if(gameStarted){
+        incrementScore();
+        if(checkForCompletion()){
+            onGameWin();
+        }
+        update();
+    }
 }
+
 function generateButton(text){
     let newButton = document.createElement("button");
     newButton.className = "numberButton";
@@ -46,10 +102,17 @@ function generateButton(text){
         newButton.style.gridRow = Math.trunc((Number.parseInt(text) - 1) / 4 + 1);
         newButton.style.gridColumn = Number.parseInt(text) - ((Number.parseInt(newButton.style.gridRow) - 1) * 4);
     } else {
-        newButton.style.gridColumn = 4
-        newButton.style.gridRow = 4
+        newButton.style.gridArea = "4 / 4";
     }
+    newButton.style.backgroundColor = "blue";
     return newButton;
 }
 
+function toggleGameStarted() {
+    gameStarted = !gameStarted;
+}
+
+function incrementScore() {
+    score ++;
+}
 initialize();
